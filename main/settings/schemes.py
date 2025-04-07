25362e3dba057c0b70921bd2126327758227206c
Nil <rdbel_ozerov@outlook.com>
2025-04-07 22:29:56 +0300

    В директории settings/ создан файл schemes.py, в который перенесены схемы для users

---
+++ main/settings/schemes.py
@@ -0,0 +1,15 @@
+# установленные модули
+from pydantic import BaseModel, Field
+
+# схема юзера для его добавления в БД
+class UserSchema(BaseModel):
+    user_name: str = Field(min_length=1, max_length=255)
+    login: str = Field(min_length=1, max_length=255)
+
+class AddUserSchema(UserSchema):
+    password: str = Field(min_length=8, max_length=100)
+
+# схема юзера для аутентификации/авторизации
+class LoginUserSchema(BaseModel):
+    login: str = Field(min_length=1, max_length=255)
+    password: str = Field(max_length=100)
