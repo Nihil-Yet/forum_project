53697a60bbba263a50f8015cfb3f6b1c743f680f
Nil <rdbel_ozerov@outlook.com>
2025-04-08 00:58:21 +0300

    Обновление содержимого БД

--- data/db_forum/db_forum_users.sql
+++ data/db_forum/db_forum_users.sql
@@ -30,7 +30,7 @@ CREATE TABLE `users` (
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Alex Di','Fix','$2b$12$ScYBgmZYzbHOtUNdJSSoB.KyuzZOG8tfIyU2n3qKvBAAvXpJgDG8.',NULL),(5,'Гарри Поттер','Выживший','$2b$12$lUTkJPXnSiv1El9r5BoICu5zwk3roQuxS5xZ3qkYDP116td7tGtFm',NULL),(6,'Vasya Pupkin','VasP','$2b$12$GGPvKlmoD7ReSwx6XaxAHOF9D46mM5wSfK/0jPtQ5kxvD5ayrYaSm',NULL),(10,'Vasya Pupkin','PiVo','$2b$12$dsHIYNu2NheCaUR./yIke.EA7s80x.LZQK6iUfA/2.OprIg.vNDk6',NULL),(11,'Vasya Pupkin','PiVk0','$2b$12$pwvz.RAQxJ4waWZ5SlsCx.hfkcsCwdwHi6QRaRz.FCKjIzXOExPkq',NULL),(13,'Петрович','Petrovich','$2b$12$.zD/iwwfSYhbZ8q8wkgLN.ZQ7QZWrnhHFSGZDQ0vFFC1yH.qj0nHy',NULL),(14,'Петрович','KKKetr','$2b$12$E7z0qZ/F2NViyMmJHCbXSeNMhBG1xsB/iTW9zsEeZRxqIxGhRSEVy',NULL),(15,'Валерия','VaL','$2b$12$SljP0YmmWY8S.7q0qXw1lu9CAPp5e0xzQPvbyiHH9PDIdIgGVuIhK',NULL),(16,'Test User','TUser','$2b$12$bV92rYyjxcLGitbc0M.Nb.nRNzfBE3t1sZefJ76M4fnk4wSyYLH5S',NULL),(19,'Favoni','Fav','$2b$12$TLZuLP9OQ6U1fSvFB1EAmeCvLtyc86O6HJxrMmWO9salDb3Pvvs0e',NULL);
+INSERT INTO `users` VALUES (20,'Test','TestUser','$2b$12$juBfEwfJBaaadgQDy4j6/u773xsE9UxexSyQZJqOYD6GDCn/kLGI2','test1user@example.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-08  0:23:24
+-- Dump completed on 2025-04-08  0:56:23
