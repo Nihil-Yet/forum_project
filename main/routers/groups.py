2d95aa1d34476b1e3041971a28c786a9b38bc1a1
Nil <rdbel_ozerov@outlook.com>
2025-04-13 19:48:00 +0300

    Добавление функции get_group

--- main/routers/groups.py
+++ main/routers/groups.py
@@ -9,26 +9,26 @@ from settings.schemes import GroupSchema
 routerGroups = APIRouter()
 
 # функция добавления группы
-@routerGroups.post("/groups/create/", tags = ["Groups"])
+@routerGroups.post("/groups/create/")
 async def add_group(new_group: GroupSchema):
     connection = None
     try:
         connection = await database_connect()
         async with connection.cursor() as cursor:
             await cursor.execute("""SELECT `id` FROM `groups` WHERE `group_name` = %s""",
-                                 (new_group.name))
+                                 (new_group.group_name))
             group_exist = await cursor.fetchone()
             if group_exist:
                 raise HTTPException(status_code=409, detail="This group name already used")
             await cursor.execute("""INSERT INTO `groups` (group_name, description) VALUES (%s, %s)""", 
-                                 (new_group.name, new_group.description))
+                                 (new_group.group_name, new_group.description))
             await connection.commit()
             return {"message": "Group added succesfully"}
     finally:
         if connection: connection.close()
 
 # функция получения информации о всех существующих группах
-@routerGroups.get("/groups/", tags = ["Groups"])
+@routerGroups.get("/groups/")
 async def get_groups():
     connection = None
     try:
@@ -39,5 +39,20 @@ async def get_groups():
             if not groups:
                 raise HTTPException(status_code = 404, detail = "groups are not found")
             return groups
+    finally:
+        if connection: connection.close()
+
+# функция получения информации о пользователе по его id
+@routerGroups.get("/groups/{group_id}/")
+async def get_group(group_id: int) -> GroupSchema:
+    connection = None
+    try:
+        connection = await database_connect()
+        async with connection.cursor() as cursor:
+            await cursor.execute("""SELECT * FROM `groups` WHERE id = %s;""", (group_id,))
+            query_result = await cursor.fetchone()
+            if not query_result:
+                raise HTTPException(status_code = 404, detail = f"group with id = {group_id} not found")
+            return GroupSchema(**query_result)
     finally:
         if connection: connection.close()
