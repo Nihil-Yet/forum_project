c7e23cfdbbd852bfd1a28e5d29244ad0d240cb9a
Nil <rdbel_ozerov@outlook.com>
2025-04-20 18:50:11 +0300

    Удален email, добавлен is_student

--- main/settings/schemes.py
+++ main/settings/schemes.py
@@ -1,11 +1,11 @@
 # установленные модули
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 
 # схема юзера для его добавления в БД
 class UserSchema(BaseModel):
     user_name: str = Field(min_length=1, max_length=255)
     login: str = Field(min_length=1, max_length=255)
-    email: EmailStr
+    is_studen: bool = 0
 
 class AddUserSchema(UserSchema):
     password: str = Field(min_length=8, max_length=100)
