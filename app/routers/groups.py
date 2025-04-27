866ff9a0634f8fa63a946cbba0c992871d4c9702
Nil <rdbel_ozerov@outlook.com>
2025-04-27 21:29:24 +0300

    Добавлена аутентификация для присоединения к группе

--- app/routers/groups.py
+++ app/routers/groups.py
@@ -77,16 +77,21 @@ async def get_group(group_id: int) -> GroupSchema:
 
 # функция вступления в группу
 @routerGroups.post("/groups/members/join/")
-async def joining_member(new_member: JoinGroupMember):
+async def joining_member(
+    new_member: JoinGroupMember,
+    user_token: UserSchema = Depends(get_jwt_payload)
+    ):
     connection = None
     try:
         connection = await database_connect()
         async with connection.cursor() as cursor:
             await cursor.execute(
                 """SELECT * FROM `users` WHERE `id` = %s""",
-                (new_member.user_id,))
+                (user_token["id"],))
             if not await cursor.fetchone():
-                raise HTTPException(status_code=404, detail=f"User with id = {new_member.user_id} not found")
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"User with id = {user_token["id"]} not found")
             await cursor.execute(
                 """SELECT * FROM `groups` WHERE `id` = %s""",
                 (new_member.group_id,))
@@ -94,7 +99,7 @@ async def joining_member(new_member: JoinGroupMember):
                 raise HTTPException(status_code=404, detail=f"Group with id = {new_member.group_id} not found")
             await cursor.execute(
                 """SELECT `id` FROM `user_group` WHERE `user_id` = %s AND `group_id` = %s;""",
-                (new_member.user_id, new_member.group_id,))
+                (user_token["id"], new_member.group_id,))
             if await cursor.fetchone():
                 raise HTTPException(
                     status_code=409,
