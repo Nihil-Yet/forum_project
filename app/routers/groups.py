b15baba5189c8d59bce86755573d4c05e0a57239
Nil <rdbel_ozerov@outlook.com>
2025-04-27 21:25:50 +0300

    Изменение компановки

--- app/routers/groups.py
+++ app/routers/groups.py
@@ -82,20 +82,26 @@ async def joining_member(new_member: JoinGroupMember):
     try:
         connection = await database_connect()
         async with connection.cursor() as cursor:
-            await cursor.execute("""SELECT * FROM `users` WHERE `id` = %s""",
-                                 (new_member.user_id,))
+            await cursor.execute(
+                """SELECT * FROM `users` WHERE `id` = %s""",
+                (new_member.user_id,))
             if not await cursor.fetchone():
                 raise HTTPException(status_code=404, detail=f"User with id = {new_member.user_id} not found")
-            await cursor.execute("""SELECT * FROM `groups` WHERE `id` = %s""",
-                                 (new_member.group_id,))
+            await cursor.execute(
+                """SELECT * FROM `groups` WHERE `id` = %s""",
+                (new_member.group_id,))
             if not await cursor.fetchone():
                 raise HTTPException(status_code=404, detail=f"Group with id = {new_member.group_id} not found")
-            await cursor.execute("""SELECT `id` FROM `user_group` WHERE `user_id` = %s AND `group_id` = %s;""",
-                                 (new_member.user_id, new_member.group_id,))
+            await cursor.execute(
+                """SELECT `id` FROM `user_group` WHERE `user_id` = %s AND `group_id` = %s;""",
+                (new_member.user_id, new_member.group_id,))
             if await cursor.fetchone():
-                raise HTTPException(status_code=409, detail="This user is already a member of this group")
-            await cursor.execute("""INSERT INTO `user_group` (user_id, group_id, role_id) VALUES (%s, %s, %s);""",
-                                 (new_member.user_id, new_member.group_id, new_member.role_id,))
+                raise HTTPException(
+                    status_code=409,
+                    detail="This user is already a member of this group")
+            await cursor.execute(
+                """INSERT INTO `user_group` (user_id, group_id, role_id) VALUES (%s, %s, %s);""",
+                (new_member.user_id, new_member.group_id, new_member.role_id,))
             await connection.commit()
             return {"message": "User has successfully joined the group"}
     except aiomysql.MySQLError as ex:
@@ -111,12 +117,17 @@ async def left_member(left_member: GroupMember):
     try:
         connection = await database_connect()
         async with connection.cursor() as cursor:
-            await cursor.execute("""SELECT `id` FROM `user_group` WHERE `user_id` = %s AND `group_id` = %s;""",
-                                 (left_member.user_id, left_member.group_id,))
+            await cursor.execute(
+                """SELECT `id` FROM `user_group` WHERE `user_id` = %s AND `group_id` = %s;""",
+                (left_member.user_id, left_member.group_id,))
             isUser = await cursor.fetchone()
             if not isUser:
-                raise HTTPException(status_code=404, detail=f"User with id = {left_member.user_id} not found in group with id = {left_member.group_id} or this group not exist")
-            await cursor.execute("""DELETE FROM `user_group` WHERE `id` = %s""", (isUser["id"],))
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"User with id = {left_member.user_id} not found in group with id = {left_member.group_id} or this group not exist")
+            await cursor.execute(
+                """DELETE FROM `user_group` WHERE `id` = %s""",
+                (isUser["id"],))
             await connection.commit()
             return {"message": "User has successfully left the group"}
     except aiomysql.MySQLError as ex:
@@ -132,13 +143,19 @@ async def get_group_members(group_id: int):
     try:
         connection = await database_connect()
         async with connection.cursor() as cursor:
-            await cursor.execute("""SELECT `id` FROM `groups` WHERE `id` = %s""", (group_id))
+            await cursor.execute(
+                """SELECT `id` FROM `groups` WHERE `id` = %s""",
+                (group_id))
             if not await cursor.fetchone():
                 raise HTTPException(status_code = 404, detail = "Group not found")
-            await cursor.execute("""SELECT `id`, `user_id`, `role_id` FROM user_group WHERE `group_id` = %s""", (group_id,))
+            await cursor.execute(
+                """SELECT `id`, `user_id`, `role_id` FROM user_group WHERE `group_id` = %s""",
+                (group_id,))
             group_members = await cursor.fetchall()
             if not group_members:
-                raise HTTPException(status_code = 404, detail = f"No members in the group with id = {group_id}")
+                raise HTTPException(
+                    status_code = 404,
+                    detail = f"No members in the group with id = {group_id}")
             return group_members
     finally:
         if connection:
@@ -151,10 +168,14 @@ async def get_group_posts(group_id: int):
     try:
         connection = await database_connect()
         async with connection.cursor() as cursor:
-            await cursor.execute("""SELECT * FROM `posts` WHERE `group_id` = %s;""", (group_id,))
+            await cursor.execute(
+                """SELECT * FROM `posts` WHERE `group_id` = %s;""",
+                (group_id,))
             query_result = await cursor.fetchall()
             if not query_result:
-                raise HTTPException(status_code = 404, detail = "Posts not found or group not exist")
+                raise HTTPException(
+                    status_code = 404,
+                    detail = "Posts not found or group not exist")
             return query_result
     finally:
         if connection: connection.close()
@@ -171,15 +192,21 @@ async def delete_group(
         async with connection.cursor() as cursor:
             await cursor.execute("""SELECT * FROM `groups` WHERE id = %s;""", (group_id,))
             if not await cursor.fetchone():
-                raise HTTPException(status_code = 404, detail = f"group with id = {group_id} not found")
+                raise HTTPException(
+                    status_code = 404,
+                    detail = f"group with id = {group_id} not found")
             await cursor.execute(
                 """SELECT * FROM `user_group` WHERE `user_id` = %s AND `group_id` = %s;""",
                 (user_token["id"], group_id,))
             user = await cursor.fetchone()
             if not user:
-                raise HTTPException(status_code = 404, detail = f"user {user_token["id"]} not in group {group_id}")
+                raise HTTPException(
+                    status_code = 404,
+                    detail = f"user {user_token["id"]} not in group {group_id}")
             if user["role_id"] != 1:
-                raise HTTPException(status_code = 403, detail = f"user {user_token["id"]} not have enough rights")
+                raise HTTPException(
+                    status_code = 403,
+                    detail = f"user {user_token["id"]} not have enough rights")
             await cursor.execute("""DELETE FROM `groups` WHERE `id` = %s;""", (group_id,))
             await connection.commit()
             return {"message": "Group delete succesfully"}
