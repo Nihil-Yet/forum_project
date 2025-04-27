3ca91b1a8a3079e673715da19c41fb2b41fc98b3
Nil <rdbel_ozerov@outlook.com>
2025-04-27 16:33:56 +0300

    Перенесена функция get_user_comments

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -68,23 +68,6 @@ async def get_post_comments(post_id: int):
         if connection: connection.close()
 
 # получения комментариев пользователя
-@routerComments.post("/comments/{user_id}/")
-async def get_user_comments(user_id: int):
-    connection = None
-    try:
-        connection = await database_connect()
-        async with connection.cursor() as cursor:
-            await cursor.execute("""SELECT * FROM `users` WHERE `id` = %s""", (user_id,))
-            if not await cursor.fetchone():
-                raise HTTPException(status_code = 404, detail = "User not found")
-            await cursor.execute("""SELECT * FROM `comments` WHERE `user_id` = %s""",
-                                 (user_id,))
-            user_comments = await cursor.fetchall()
-            return user_comments
-    finally:
-        if connection: connection.close()
-
-# получения комментариев пользователя
 @routerComments.delete("/comments/{comment_id}/")
 async def delete_comment(comment_id: int):
     connection = None
--- app/routers/users.py
+++ app/routers/users.py
@@ -194,6 +194,24 @@ async def get_user_posts(user_id: int):
     finally:
         if connection: connection.close()
 
+# получения комментариев пользователя
+@routerUsers.post("/{user_id}/comments/")
+async def get_user_comments(user_id: int):
+    connection = None
+    try:
+        connection = await database_connect()
+        async with connection.cursor() as cursor:
+            await cursor.execute("""SELECT * FROM `users` WHERE `id` = %s""", (user_id,))
+            if not await cursor.fetchone():
+                raise HTTPException(status_code = 404, detail = "User not found")
+            await cursor.execute(
+                """SELECT * FROM `comments` WHERE `user_id` = %s""",
+                (user_id,))
+            user_comments = await cursor.fetchall()
+            return user_comments
+    finally:
+        if connection: connection.close()
+
 # функция удаления пользователя по его id
 @routerUsers.delete("/users/{user_id}/")
 async def delete_user(user_id: int):
