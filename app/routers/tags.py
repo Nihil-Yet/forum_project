977af1074c4db8f919b72f116f11bdef92c07b32
Nil <rdbel_ozerov@outlook.com>
2025-04-30 18:55:35 +0300

    Добавлена функция создания тега

--- app/routers/tags.py
+++ app/routers/tags.py
@@ -8,3 +8,18 @@ from settings.database import database_connect
 from settings.schemes import PostSchema, AddPostSchema, UserSchema
 
 routerTags = APIRouter()
+
+@routerTags.post("/tags/create/{tag_name}/")
+async def create_tag(tag_name: str):
+    connection = None
+    try:
+        connection = await database_connect()
+        async with connection.cursor() as cursor:
+            await cursor.execute(
+                """SELECT * FROM `tags` WHERE `tag_name` = %s""", 
+                (tag_name,))
+            if cursor.fetchone():
+                raise HTTPException(
+                    status_code=409, detail="This tag alredy exist")
+    finally:
+        if connection: connection.close()
