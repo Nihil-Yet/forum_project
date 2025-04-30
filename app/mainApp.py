70291638e4105a27c06f72f3261dd6e1d9143ca2
Nil <rdbel_ozerov@outlook.com>
2025-04-30 19:05:49 +0300

    Добавлены эндпоинты для тегов

--- app/mainApp.py
+++ app/mainApp.py
@@ -10,6 +10,7 @@ from routers.users import routerUsers
 from routers.groups import routerGroups
 from routers.posts import routerPosts
 from routers.comments import routerComments
+from routers.tags import routerTags
 
 # Приложение
 app = FastAPI()
@@ -31,6 +32,7 @@ app.include_router(router=routerUsers, prefix="/api", tags=["Users"])
 app.include_router(router=routerGroups, prefix="/api", tags=["Groups"])
 app.include_router(router=routerPosts, prefix="/api", tags=["Posts"])
 app.include_router(router=routerComments, prefix="/api", tags=["Comments"])
+app.include_router(router=routerTags, prefix="/api", tags=["Tags"])
 
 if __name__ == "__main__":
     uvicorn.run("mainApp:app", reload = True)
