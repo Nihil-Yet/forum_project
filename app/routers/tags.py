c17d88611c0f17d0acc174b4ee801bdd54ba5e30
Nil <rdbel_ozerov@outlook.com>
2025-04-30 19:00:05 +0300

    Добавлена функция получения списка всех тегов

--- app/routers/tags.py
+++ app/routers/tags.py
@@ -18,8 +18,22 @@ async def create_tag(tag_name: str):
             await cursor.execute(
                 """SELECT * FROM `tags` WHERE `tag_name` = %s""", 
                 (tag_name,))
-            if cursor.fetchone():
+            if await cursor.fetchone():
                 raise HTTPException(
                     status_code=409, detail="This tag alredy exist")
+    finally:
+        if connection: connection.close()
+
+@routerTags.get("/tags/")
+async def get_tags():
+    connection = None
+    try:
+        connection = await database_connect()
+        async with connection.cursor() as cursor:
+            await cursor.execute("""SELECT * FROM `tags`""")
+            tags = cursor.fetchall()
+            if not tags:
+                raise HTTPException(
+                    status_code=404, detail="Tags not found")
     finally:
         if connection: connection.close()
