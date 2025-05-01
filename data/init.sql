828833e3c89d65d9d3f0d3e41b7a2c1343b460a8
Nil <rdbel_ozerov@outlook.com>
2025-05-01 09:14:47 +0500

    Исправлено имя поля на creation_time

--- data/init.sql
+++ data/init.sql
@@ -95,7 +95,7 @@ CREATE TABLE `comments` (
   `user_id` int NOT NULL,
   `post_id` int NOT NULL,
   `comment_text` text,
-  `creation_date` DATETIME,
+  `creation_time` DATETIME,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `post_id` (`post_id`),
