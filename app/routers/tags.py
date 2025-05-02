4e6c5db01e93a5cc17f944a72964450cd63bf9b7
Nil <rdbel_ozerov@outlook.com>
2025-05-02 14:42:44 +0500

    Удалены не используемые импорты

--- app/routers/tags.py
+++ app/routers/tags.py
@@ -1,11 +1,8 @@
 # установленные модули
-import aiomysql
 from fastapi import APIRouter, HTTPException, Depends
-import logging
 
 # собственные модули
 from settings.database import database_connect
-from settings.schemes import PostSchema, AddPostSchema, UserSchema
 
 routerTags = APIRouter()
 
